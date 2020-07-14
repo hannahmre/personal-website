5ff82af55be4a568426c5102268a7961d8d7c88d
hannahmre <hcm8434@uncw.edu>
2020-07-13 22:42:47 -0400

    fade animation

--- scripts/transition.js
+++ scripts/transition.js
@@ -5,20 +5,15 @@ var boxes = document.getElementsByClassName('contentbox');
 function content_change(tab) {
 
     if (tab == 'db') {
-        // document.body.style.backgroundColor = "#ffccff";
         transition(0);
 
     } else if (tab == 'wp') {
-        // document.body.style.backgroundColor = "#ccccff";
         transition(1);
 
     } else if (tab == 'vr') {
-
-        // document.body.style.backgroundColor = "green";
         transition(2);
 
     } else {
-        // document.body.style.backgroundColor = "black";
         transition(3);
 
     }
@@ -28,10 +23,8 @@ function content_change(tab) {
 function transition(num) {
     for (var b = 0; b < buttons.length - 1; b++) {
         if (b == num) {
-            // console.log('here1');
             boxes[num].style.display = 'block';
             buttons[num].classList.add = 'active';
-            // console.log('here2');
         } else {
             boxes[b].style.display = 'none';
             buttons[b].classList.remove = 'active';
